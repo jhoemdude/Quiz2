9ead46c8136f0b0ebf764b8cc9aa30109c02bd5f
Princess Joem Perida <princessjoem.perida@jmc.edu.ph>
2016-03-09 10:21:24 +0800

    Prefinal-Quiz2
    All about Quiz II

---
+++ Quiz2/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quiz2
+{
+    public partial class Quiz2 : Form
+    {
+        public Quiz2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This is a settings generated from the app");
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("This is a settings generated from the app.")
+        }
+    }
+}
